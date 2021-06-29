10c7a5b34f3bc911cc80da9cf4cc8286c6494b35
Daniel Soto <dasg.1993@gmail.com>
2021-06-28 22:04:00 -0500

    [faeture] - implement core components react native

--- App.js
+++ App.js
@@ -7,7 +7,8 @@
  */
 
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import Logo from './src/shared/static/image/react-native.png';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,11 +20,22 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '800',
   },
+  image: {
+    width: 200,
+    height: 200,
+  },
 });
 
 const App = () => {
   return (
     <View style={styles.container}>
+      {/* <Image
+        source={{
+          uri: 'https://s3-eu-west-1.amazonaws.com/xavitristancho/react-native.png',
+        }}
+        style={styles.image}
+      /> */}
+      <Image source={Logo} style={styles.image} />
       <Text style={styles.title}>Hello World</Text>
     </View>
   );
